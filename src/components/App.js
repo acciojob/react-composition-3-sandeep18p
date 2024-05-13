4e8f65577df079efb989fefd434597cda814ccc3
sandeep18p <sandeep.pansari98@gmail.com>
2024-05-13 16:08:50 +0530

    4

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,18 @@
-
 import React from "react";
-import './../styles/App.css';
+import './../styles/App.css'; // Assuming you have a separate CSS file for App styling
 import Tooltip from "./Tooltip";
 
 const App = () => {
   return (
     <>
-    <div className="one">
-        <Tooltip text="This is a tooltip"><h1>Hover over me</h1></Tooltip>
-    </div>
-    <div className="one">
-        <Tooltip text="Hover over me to see another tooltip"><h1>Hover over me</h1></Tooltip>
-    </div>
-     
+      <div className="one">
+          <Tooltip text="This is a tooltip"><h1>Hover over me</h1></Tooltip>
+      </div>
+      <div className="one">
+          <Tooltip text="Hover over me to see another tooltip"><h1>Hover over me</h1></Tooltip>
+      </div>
     </>
   )
 }
 
-export default App
+export default App;
--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,33 +1,24 @@
 // Tooltip.js
 import React, { useState } from 'react';
-// import './Tooltip.css'; // Import CSS file for styling
-import './../styles/App.css';
+import './Tooltip.css'; // Import CSS for styling
 
 const Tooltip = ({ text, children }) => {
-    const [showTooltip, setShowTooltip] = useState(false);
-  
-    const handleMouseEnter = () => {
-      setShowTooltip(true);
-      console.log('hello')
-    };
-  
-    const handleMouseLeave = () => {
-      setShowTooltip(false);
-    };
-  
-    return (
-      <div className="tooltip-container">
-        {showTooltip && <div className="tooltiptext">{text}</div>}
-        <div
-          className="tooltip"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        >
-          {children}
-        </div>
-        
-      </div>
-    );
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowTooltip(true);
   };
-  
-  export default Tooltip;
+
+  const handleMouseLeave = () => {
+    setShowTooltip(false);
+  };
+
+  return (
+    <div className="tooltip-container" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <div className="tooltip">{children}</div>
+      {showTooltip && <div className="tooltiptext">{text}</div>}
+    </div>
+  );
+};
+
+export default Tooltip;
